55fe072bda78d4b44d1eba3c1d9ee4adb4cc1200
hungnguyen1804 <nguyendinhhung1804@gmail.com>
2021-12-07 23:01:42 +0700

    add fn halt

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,22 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+/* Hung commit code */
+#include "threads/vaddr.h"
+#include "threads/init.h"
+#include "userprog/process.h"
+#include <list.h>
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+#include "threads/palloc.h"
+#include "threads/malloc.h"
+#include "devices/input.h"
+#include "threads/synch.h"
+
+static int syscall_halt (void);
+/* == Hung commit code */
+
+
 
 static void syscall_handler (struct intr_frame *);
 
@@ -18,3 +34,12 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+/* Hung commit code */
+static int
+syscall_halt() {
+  power_off ();
+}
+/* == Hung commit code */
+
+
+
